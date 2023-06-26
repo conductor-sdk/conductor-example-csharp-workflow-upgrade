7070cefdf320801f53a9669bbf5c900652518241
manan164 <1897158+manan164@users.noreply.github.com>
2023-06-27 00:47:32 +0530

    update task api

--- WorkflowMigration/WorkflowMigration.cs
+++ WorkflowMigration/WorkflowMigration.cs
@@ -1,43 +1,12 @@
 ﻿
 using Conductor.Api;
 using Conductor.Client;
-using Conductor.Client.Authentication;
-using Conductor.Client.Extensions;
-using Conductor.Client.Interfaces;
-using Conductor.Client.Worker;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Conductor.Client.Models;
-using TestOrkesSDK;
 using static Conductor.Client.Models.TaskResult;
+using Conductor.Client.Authentication;
 
-
-class ConsumerExample
+class WorkflowMigration
 {
-    static Dictionary<string, Conductor.Client.Models.Task>? oldWorkflowTaskMapping;
-
-    internal class SimpleWorkerNew : IWorkflowTask
-    {
-        public string TaskType { get; }
-        public WorkflowTaskExecutorConfiguration WorkerSettings { get; }
-
-        public SimpleWorkerNew(string taskType = "test-sdk-csharp-task")
-        {
-            TaskType = taskType;
-            WorkerSettings = new WorkflowTaskExecutorConfiguration();
-            WorkerSettings.Domain = "after_migration";
-        }
-
-        public TaskResult Execute(Conductor.Client.Models.Task task)
-        {
-            Conductor.Client.Models.Task oldTask = oldWorkflowTaskMapping[task.WorkflowInstanceId + ":" + task.ReferenceTaskName];
-            TaskResult taskResult = new TaskResult(taskId:task.TaskId, workflowInstanceId: task.WorkflowInstanceId);
-            taskResult.Status = StatusEnum.COMPLETED;
-            taskResult.OutputData = oldTask.OutputData;
-            taskResult.TaskId = task.TaskId;
-            return taskResult;
-        }
-    }
 
     private static Configuration configuration = new Configuration()
     {
@@ -48,99 +17,81 @@ class ConsumerExample
     public static void Main(string[] args)
     {
 
-        /*
-         * 1. Search all the running workflows for the given the type.
-         * 2. Terminate all the workflows and store the workflow execution information.
-         * 3. Trigger new workflows where the workflow input contains the old workflowId. Domain must be set for all old taskref names to old.
-         * 4. Create a map of new workflow run to old workflow run. and poll for the task. When the task comes, find the workflowId. From the map find the old workflowId and using the task refname set the task output.
-         */
+        string workflowId = args[0];
+        string workflowName = args[1];
+        int workflowVersion = args[2] != null ? int.Parse(args[2]) : 0;
 
-        List<string> workflowNames = new List<string>
-            {
-            "csharp"
-            };
+        // This is the map of new task ref names to expected output.
+        var dictionary = args[3].Select(a => a.ToString().Split('='))
+                     .ToDictionary(a => a[0], a => a.Length == 2 ? a[1] : null);
+
         WorkflowResourceApi workflowResourceApi = new WorkflowResourceApi(configuration);
+        TaskResourceApi taskResourceApi = new TaskResourceApi(configuration);
 
+        Dictionary<string, Conductor.Client.Models.Task> oldWorkflowTaskMapping = new Dictionary<string, Conductor.Client.Models.Task>();
+        HashSet<string> taskNames = new HashSet<string>();
 
-        ScrollableSearchResultWorkflowSummary scrollableSearchResultWorkflowSummary = workflowResourceApi.Search(query: "workflowType IN (" + workflowNames[0] + ") AND status IN (RUNNING)", skipCache:true);
-
-        Dictionary<string, Workflow> oldWorkflowMapping = new Dictionary<string, Workflow>();
-        Dictionary<string, string> workflowsMapping = new Dictionary<string, string>();
-        oldWorkflowTaskMapping = new Dictionary<string, Conductor.Client.Models.Task>();
-        HashSet<string> workerNames = new HashSet<string>();
-
-        // Create task mapping. So for the new task we just have to get the old workflowId and related task using below map.
-        // Trigger new workflow with old one as input.
-        scrollableSearchResultWorkflowSummary.Results.ForEach(workflowSummary => {
-            StartWorkflowRequest startWorkflowRequest = new StartWorkflowRequest(name:workflowSummary.WorkflowType);
-
-            Dictionary<string, object> input = new Dictionary<string, object>();
-            input["_rerunFromWorkflowId"] = workflowSummary.WorkflowId;
-
-            startWorkflowRequest.Input = input;
-            Dictionary<string, string> domain = new Dictionary<string, string>();
-            domain["*"] = "after_migration";
-            startWorkflowRequest.TaskToDomain = domain;
-            String newWorkflowId = workflowResourceApi.StartWorkflow(startWorkflowRequest);
-            workflowsMapping[newWorkflowId] = workflowSummary.WorkflowId;
-
-
-            Workflow workflow = workflowResourceApi.GetExecutionStatus(workflowSummary.WorkflowId, true);
-            oldWorkflowMapping[workflowSummary.WorkflowId] = workflow;
-            workflow.Tasks.ForEach(task =>
+        Workflow workflow = workflowResourceApi.GetExecutionStatus(workflowId, true);
+        workflow.Tasks.ForEach(task =>
+        {
+            if (task.TaskType.Equals(task.ReferenceTaskName) && task.Status.Equals(Conductor.Client.Models.Task.StatusEnum.COMPLETED))
             {
-                if (task.GetType().Equals(task.ReferenceTaskName))
+                taskNames.Add(task.ReferenceTaskName);
+                oldWorkflowTaskMapping[task.ReferenceTaskName] = task;
+            }
+        });
+
+        StartWorkflowRequest startWorkflowRequest = new StartWorkflowRequest(name: workflowName, version: workflowVersion == 0 ? null : workflowVersion);
+
+        Dictionary<string, object> input = new Dictionary<string, object>();
+        input["_rerunFromWorkflowId"] = workflowId;
+
+        startWorkflowRequest.Input = input;
+        Dictionary<string, string> domain = new Dictionary<string, string>();
+        taskNames.ToList().ForEach(taskName =>
+        {
+            domain[taskName] = System.Guid.NewGuid().ToString();
+        });
+        startWorkflowRequest.TaskToDomain = domain;
+        string newWorkflowId = workflowResourceApi.StartWorkflow(startWorkflowRequest);
+
+        workflowResourceApi.Terminate(workflowId, "Terminated because of migration. A new workflow " + newWorkflowId +" has been started");
+
+        Workflow newWorkflow = workflowResourceApi.GetExecutionStatus(newWorkflowId, true);
+
+        while(!newWorkflow.Status.Equals(WorkflowStatus.StatusEnum.COMPLETED))
+        {
+            newWorkflow = workflowResourceApi.GetExecutionStatus(newWorkflowId, true);
+            List<Conductor.Client.Models.Task> inProgressTask = newWorkflow.Tasks.FindAll(task =>
+                                                                                        task.Status.Equals(Conductor.Client.Models.Task.StatusEnum.INPROGRESS) ||
+                                                                                        task.Status.Equals(Conductor.Client.Models.Task.StatusEnum.SCHEDULED));
+            inProgressTask.ForEach(task =>
+            {
+                if (oldWorkflowTaskMapping.ContainsKey(task.ReferenceTaskName))
                 {
-                    workerNames.Add(task.ReferenceTaskName);
-                    oldWorkflowTaskMapping[newWorkflowId + ":" + task.ReferenceTaskName] = task;
+                    TaskResult taskResult = new TaskResult(taskId: task.TaskId, workflowInstanceId: task.WorkflowInstanceId);
+                    taskResult.Status = StatusEnum.COMPLETED;
+                    taskResult.OutputData = oldWorkflowTaskMapping[task.ReferenceTaskName].OutputData;
+                    taskResult.TaskId = task.TaskId;
+                    taskResourceApi.UpdateTask(taskResult);
+                    Console.WriteLine("Task " + task.ReferenceTaskName + " from the workflowId " + newWorkflowId + " has been marked Completed ");
+                } else if (dictionary.ContainsKey(task.ReferenceTaskName))
+                {
+                    TaskResult taskResult = new TaskResult(taskId: task.TaskId, workflowInstanceId: task.WorkflowInstanceId);
+                    taskResult.Status = StatusEnum.COMPLETED;
+                    Dictionary<string, object> output = new Dictionary<string, object>();
+                    output["migration_output"] = dictionary[task.ReferenceTaskName] ?? "no_output";
+                    taskResult.OutputData = output;
+                    taskResult.TaskId = task.TaskId;
+                    taskResourceApi.UpdateTask(taskResult);
+                    Console.WriteLine("Task " + task.ReferenceTaskName + " from the workflowId " + newWorkflowId + " has been marked Completed ");
+                } else
+                {
+                    throw new Exception("Task " + task.ReferenceTaskName + " output not provided");
                 }
             });
-
-            workflowResourceApi.Terminate(workflowSummary.WorkflowId, "Terminated because of migration. A new workflow " + newWorkflowId +" has been started");
-
-        });
-
-
-        GetWorkerHost(workerNames);
-        Console.ReadLine();
-    }
-
-    public static TaskResult Execute(Conductor.Client.Models.Task task)
-    {
-        Conductor.Client.Models.Task oldTask = oldWorkflowTaskMapping[task.WorkflowInstanceId + ":" + task.ReferenceTaskName];
-        TaskResult taskResult = new TaskResult(taskId: task.TaskId, workflowInstanceId: task.WorkflowInstanceId);
-        taskResult.Status = StatusEnum.COMPLETED;
-        taskResult.OutputData = oldTask.OutputData;
-        taskResult.TaskId = task.TaskId;
-        return taskResult;
-    }
-
-    private static object GetWorkerHost(HashSet<string> workerNames)
-    {
-        List<GenericWorker> workerTasks = new List<GenericWorker>();
-        workerNames.ToList().ForEach(workerName =>
-        {
-            WorkflowTaskExecutorConfiguration workflowTaskExecutorConfiguration = new WorkflowTaskExecutorConfiguration();
-            GenericWorker workerTask = new GenericWorker(taskType: workerName, workflowTaskExecutorConfiguration, executeTaskMethod: );
-            workerTasks.Add(workerTask);
-        });
-        return new HostBuilder()
-    .ConfigureServices(
-        (ctx, services) =>
-        {
-            services.AddConductorWorker(configuration);
-            workerTasks.ForEach(worker =>
-            {
-                services.AddConductorWorkflowTask(worker);
-            });
-            services.WithHostedService<WorkerService>();
+            newWorkflow = workflowResourceApi.GetExecutionStatus(newWorkflowId, true);
         }
-    ).ConfigureLogging(
-        logging =>
-        {
-            logging.SetMinimumLevel(LogLevel.Debug);
-            logging.AddConsole();
-        }
-    ).Build().RunAsync();
+        Console.WriteLine("Workflow workflowId " + newWorkflowId + " Completed Successfully");
     }
 }
